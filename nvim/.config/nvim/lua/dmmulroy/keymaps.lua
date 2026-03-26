8ac60d6c1e97753c9e3839767a9509922c02043e
Aayush Bajaj <aayushbajaj7@gmail.com>
2026-03-26 13:20:06 +1100

    enh: add <SPC>Yf to yank full file path to clipboard in nvim

--- nvim/.config/nvim/lua/dmmulroy/keymaps.lua
+++ nvim/.config/nvim/lua/dmmulroy/keymaps.lua
@@ -165,6 +165,10 @@ end, { desc = "Open diagnostic float with rounded border" })
 
 vim.keymap.set("n", "<leader>cd", copy_line_diagnostics_to_clipboard, { desc = "[C]opy line [D]iagnostics" })
 
+vim.keymap.set("n", "<leader>Yf", function()
+	vim.fn.setreg("+", vim.api.nvim_buf_get_name(0))
+end, { desc = "[Y]ank [F]ile full path to clipboard" })
+
 vim.keymap.set("n", "<leader>ld", vim.diagnostic.setqflist, { desc = "Populate quickfix list with diagnostics" })
 
 -- Quickfix navigation
