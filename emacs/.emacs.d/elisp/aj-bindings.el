d603d8d42305552d323193d8456bf6df11a3e4aa
Aayush Bajaj <aayushbajaj7@gmail.com>
2026-04-22 04:14:53 +1000

    enh: add C-c b p to deploy beancount ledger to ledger.abaj.ai

--- emacs/.emacs.d/elisp/aj-bindings.el
+++ emacs/.emacs.d/elisp/aj-bindings.el
@@ -39,6 +39,29 @@ In dired buffers, copies the current directory path."
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "V") #'aj/dired-open-in-sioyek))
 
+;; ---------------------------------------------------------------------------
+;; Beancount: instant deploy to ledger.abaj.ai
+;; ---------------------------------------------------------------------------
+
+(defconst aj/finances-dir "/Users/aayushbajaj/Documents/Finances/beancount/"
+  "Root of the beancount ledger repository.")
+
+(defun aj/beancount-deploy ()
+  "Validate, commit, push, and trigger remote pull of the ledger.
+Bypasses the hourly sync cron so ledger.abaj.ai updates immediately."
+  (interactive)
+  (save-some-buffers t
+                     (lambda ()
+                       (and buffer-file-name
+                            (string-prefix-p aj/finances-dir buffer-file-name))))
+  (let ((default-directory aj/finances-dir)
+        (buf "*beancount-deploy*"))
+    (with-current-buffer (get-buffer-create buf)
+      (let ((inhibit-read-only t)) (erase-buffer)))
+    (async-shell-command (concat aj/finances-dir "deploy.sh") buf)))
+
+(define-key global-map (kbd "C-c b p") #'aj/beancount-deploy)
+
 (provide 'aj-bindings)
 
 ;;; aj-bindings.el ends here
